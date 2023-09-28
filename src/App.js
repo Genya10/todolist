6c22a200be896788378b1ca67f53a76afa398759
Jenya <113167825+Genya10@users.noreply.github.com>
2023-09-28 12:47:34 +0300

    Update App.js
    add bro

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello,everybody,friends
+         Hello,everybody,friends Bro bro bro
         </p>
         <a
           className="App-link"
